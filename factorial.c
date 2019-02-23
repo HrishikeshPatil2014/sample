c0244fbd618b3dff19d33e65cbcea8e63f908fb8
HrishikeshPatil2014 <47878096+HrishikeshPatil2014@users.noreply.github.com>
2019-02-23 23:49:27 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int number,j,k;
+int fact = 1;
+int *ptr;
+
+k=j/0;
+
+  printf("Enter a number to calculate its factorial\n");
+  scanf("%d", &number);
+
+  printf("%d! = %ld\n", number, factorial(number));
+
+  return 0;
+
+}
+int factorial(int n)
+{
+  int c;
+  int result = 1;
+
+  for (c = 1; c <= n; c++)
+    result = result * c;
+
+  return &result;
+}
